1e31e773aa8ef7a761db9447d1936175734cd17a
bestyoriya <gagaoxo5678@gmail.com>
2022-08-08 20:05:05 +0700

    Lab

--- Lab4/Lab4.c
+++ Lab4/Lab4.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
-int main(){
+int main(){   
+    char   ID[10] = "" ;
+    int    WorkingHrs ;
+    float  Amount ;
+    float  salary ;
+  
+    printf( "Input the Employees ID (Max. 10 chars) : " ) ;
+    scanf( "%s", &ID ) ;
+    printf( "Input the Working hrs : " ) ;
+    scanf( "%d", &WorkingHrs ) ;
+    printf( "Salary amount/hr (Bath) : " ) ;
+    scanf( "%f", &salary ) ;
+    printf( "--\nExpected Output :\n" ) ;
+    printf( "Employees ID = %s\n", ID ) ;
+    Amount = salary*WorkingHrs ;
+    printf( "Amount/day = %.2f Bath(s)", Amount) ;
 
-
-
-
-
-
-
-
-
-
-}//end function
+  return 0 ;
+  }//end function 
