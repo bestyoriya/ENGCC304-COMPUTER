3203d2849274e266bdc9cabcb9d947946d693a6e
bestyoriya <gagaoxo5678@gmail.com>
2022-08-30 00:29:53 +0700

    lab

--- Lab7/Lab7.c
+++ Lab7/Lab7.c
@@ -1,29 +1,22 @@
 #include<stdio.h>
 int main(){
-    int num ; 
-    //ค่าเริ่มต้นที่จะนับจนถึง0
-    int j ; 
-    //ค่าที่ใช้ในการเช็คค่าจำนวนเฉพาะ
-    int rep = 0 ; 
-    //จำนวนครั้งที่ค่าnum/ค่าjได้ลงตัว
+    int num ; //ค่าเริ่มต้นที่จะนับจนถึง0
+    int j ; //ค่าที่ใช้ในการเช็คค่าจำนวนเฉพาะ
+    int rep = 0 ; //จำนวนครั้งที่ค่าnum/ค่าjได้ลงตัว
     printf( "Input : " ) ;
     scanf( "%d", &num ) ;
-    while( num >= 2 )
-    {
+    while( num >= 2 ){
         j = num ;
-        while ( j >= 2 )
-        {
-            if ( num%j == 0 )
-            {
+        while ( j >= 2 ){
+            if ( num%j == 0 ){
                 rep++ ;
             }
             j-- ;
         }
-        if ( rep <= 1 )
-        {
+        if ( rep <= 1 ){
             printf( "%d ", num ) ;
         }
         rep = 0 ;
         num-- ;
     }
-}
+}//end function
